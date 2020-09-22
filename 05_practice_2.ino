1c0772de06ec39eb69782c4695ed3ece5a853f92
sys010611 <61095808+sys010611@users.noreply.github.com>
2020-09-23 08:13:48 +0900

    led 깜박거리기

---
+++ 05_practice_2.ino
@@ -0,0 +1,38 @@
+#define PIN_LED 7
+
+void setup()
+{
+  pinMode(PIN_LED, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(PIN_LED, 0);
+  delay(1000);
+  digitalWrite(PIN_LED, 1); //처음 1초
+  
+  digitalWrite(PIN_LED, 0);
+  delay(100);
+  digitalWrite(PIN_LED, 1); 
+  delay(100);
+  digitalWrite(PIN_LED, 0);
+  delay(100);
+  digitalWrite(PIN_LED, 1);
+  delay(100);
+  digitalWrite(PIN_LED, 0);
+  delay(100);
+  digitalWrite(PIN_LED, 1); 
+  delay(100);
+  digitalWrite(PIN_LED, 0);
+  delay(100);
+  digitalWrite(PIN_LED, 1); 
+  delay(100);
+  digitalWrite(PIN_LED, 0);
+  delay(100);
+  digitalWrite(PIN_LED, 1); 
+  while(1)
+  {
+    digitalWrite(PIN_LED, 1);
+  }
+  
+}
