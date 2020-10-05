4a5732f78a7d9857d4bf9cc8e86168736c5cebf4
sys010611 <61095808+sys010611@users.noreply.github.com>
2020-10-05 12:43:33 +0900

    코드 수정

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -1,22 +1,23 @@
 #define PIN_LED 7
 
-void setup()
-{
+void setup(){
   pinMode(PIN_LED, OUTPUT);
 }
 
-void loop()
-{
+void loop(){
   digitalWrite(PIN_LED, 0);
   delay(1000);
-  digitalWrite(PIN_LED, 1); //처음 1초
+  digitalWrite(PIN_LED, 1); 
+  delay(100);
   
   for(int i = 0; i < 5; i++){
     digitalWrite(PIN_LED, 0);
+    delay(100);
     digitalWrite(PIN_LED, 1);
+    delay(100);
   }
-  while(1)
-  {
+  
+  while(1){
     digitalWrite(PIN_LED, 1);
   }
   
